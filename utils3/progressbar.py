976d784845adcec456f422e3f377773073634d90
j_berendt <jeremy.berendt@rolls-royce.com>
2018-03-06 16:01:34 +0000

    0.1.0 - Cleaned and updated progressbar module for Py3.
    - Updated to move the 'total number of items' from the inner
    update_progress function to the instantiation.
    NOTE: This **will break** current implementations.  A minor update
    will be required to your program.
    - Minor docstring clarifications and updates.
    - Revised the user color to ANSI lookup process.

--- utils3/progressbar.py
+++ utils3/progressbar.py
@@ -1,36 +1,32 @@
-"""---------------------------------------------------------------------
+"""------------------------------------------------------------------------------------------------
 Program:    progressbar
 
-Purpose:    This is a class which provides access to a simple console
-            progress bar.
-
-Dependents: sys
+Purpose:    This is a small class module which provides access to a simple console progress bar.
 
 Developer:  J. Berendt
 Email:      support@73rdstreetdevelopment.co.uk
 
-Comments:   This class is to be incorporated into the utils package
-            later.
-
 Use:        Refer to USE section of the docstring.
 
-------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 UPDATE LOG:
 Date        Programmer      Version     Update
-05.03.18    M. Critchard    0.0.1       Permanently branched for
-                                        Python 3 from the Python 2.7
+05.03.18    M. Critchard    1.0.0       Permanently branched for Python 3 from the Python 2.7
                                         utils module.
----------------------------------------------------------------------"""
+06.03.18    J. Berendt      1.1.0       Minor docstring clarifications and updates.
+                                        Revised the user color to ANSI lookup process.
+                                        Updated to move the 'total number of items' from the
+                                        inner update_progress function to the instantiation.
+                                        NOTE: This **will break** current implementations.  A minor
+                                        update will be required to your program.
+------------------------------------------------------------------------------------------------"""
 
-# BUILT-IN IMPORTS
 import sys
-
-# EXTERNAL IMPORTS
 import colorama
 
+# TODO: Update to offer background colour.
 
 class ProgressBar(object):
-
     """
     DESIGN:
     This is a simple console progress bar which should be called
@@ -40,71 +36,70 @@ class ProgressBar(object):
     you want to configure the appearance a little bit.
 
     PARAMETERS:
-        - bar_len (default=50)
-        Length of the progress bar.
-        - symbol (default='.')
-        The symbol which is used to track progress.
-        - color (default='white')
-        Color of the progress bar.
-            - red, green, yellow, blue, magenta, cyan, white
+    - total_values (default=50)
+    Integer value defining the total iterations.
+    - bar_len (default=50)
+    Integer value defining the length of the progress bar, in chars.
+    - symbol (default='.')
+    The symbol which is used to track progress.
+    - color (default='white')
+    Color of the progress bar; where only the first letter of to colour
+    is required.
+        COLOUR OPTIONS:
+        - red, green, yellow, blue, magenta, cyan, white
 
     USE:
-    > import progressbar
+    > from utils3.progressbar import ProgressBar
     >
-    > pb = progressbar.ProgressBar(bar_len=25, symbol='#', color='r')
+    > pb = ProgressBar(total_values=25, bar_len=25, symbol='#',
+    >                  color='red')
     >
     > # SOME PROCESS
     > for i range(26):
     >     # UPDATE PROGRESS
-    >     pb.update_progress(current=i, total=25)
+    >     pb.update_progress(current=i)
     >     # SOME PAUSE TO SEE UPDATES
     >     time.sleep(.1)
     """
 
-    # CONSTANT DECLARATIONS
-    RED     = '31'
-    GREEN   = '32'
-    YELLOW  = '33'
-    BLUE    = '34'
-    MAGENTA = '35'
-    CYAN    = '36'
-    WHITE   = '37'
-
-
     # ------------------------------------------------------------------
-    def __init__(self, bar_len=50, symbol='.', color='w'):
-        """
-        PURPOSE:
-        Initialisation method.  Create the progress bar object.
-        """
+    def __init__(self, total_values=25, bar_len=25, symbol='.', color='w'):
+        """Create the progress bar object."""
 
         # MAKE COLORS WORK WITH WINDOWS
         colorama.init()
 
         # SET CLASS VARIABLES
+        self._total   = total_values
         self._bar_len = bar_len
-        self._symbol = symbol
-        self._color = color
-        self._reset = '\x1b[0m'
+        self._symbol  = symbol
+        self._color   = color
+        self._reset   = '\x1b[0m'
 
         # GET USER'S COLOUR
         self._clr = self._getcolor()
 
 
     # ------------------------------------------------------------------
-    def update_progress(self, current, total):
+    def update_progress(self, current):
         """
-        PURPOSE:
-        Method used to update the progress bar.
+        Incrementally update the progress bar.
+
+        PARAMETERS
+        - current
+        Integer value for the current iteration.
+        This value is compared against the initialised total_values
+        parameter to determine the current position in the overall
+        progress.
 
         USE:
         Refer to the class docstring: help(progressbar.ProgressBar)
         """
 
         # CALCULATE PERCENT COMPLETE
-        percent = float(current) / total
+        percent = float(current) / self._total
         # DETERMINE NUMBER OF 0 PLACEHOLDERS
-        vals = len(str(total))
+        vals = len(str(self._total))
         # NUMBER OF TICKS
         ticks = self._symbol * int(round(percent * self._bar_len))
         # NUMBER OF SPACE PLACEHOLDERS
@@ -113,7 +108,7 @@ class ProgressBar(object):
         # PRINT OUTPUT
         sys.stdout.write(self._clr + '\rProcessing %s of %s [ %s ] %.0f%% Complete' % \
                          (str(current).zfill(vals),
-                          total,
+                          self._total,
                           ticks + spaces,
                           percent*100) + self._reset)
 
@@ -124,27 +119,28 @@ class ProgressBar(object):
     # ------------------------------------------------------------------
     def _getcolor(self):
         """
-        PURPOSE:
-        This is a helper function for returning the ANSI color string
-        for the user's color option, using the global color constants.
+        Assign the ANSI colour string for the user's colour, using
+        the colour constants.
         """
 
+        # ANSI COLOUR CONSTANTS
+        RED     = '31'
+        GREEN   = '32'
+        YELLOW  = '33'
+        BLUE    = '34'
+        MAGENTA = '35'
+        CYAN    = '36'
+        WHITE   = '37'
+        # INITIALISE
         color = ''
 
         # RETURN USER COLOR AN ANSI STRING
-        if self._color.lower() == 'r' or self._color.lower() == 'red':
-            color = '\x1b[%s;40m' % (self.RED)
-        if self._color.lower() == 'g' or self._color.lower() == 'green':
-            color = '\x1b[%s;40m' % (self.GREEN)
-        if self._color.lower() == 'y' or self._color.lower() == 'yellow':
-            color = '\x1b[%s;40m' % (self.YELLOW)
-        if self._color.lower() == 'b' or self._color.lower() == 'blue':
-            color = '\x1b[%s;40m' % (self.BLUE)
-        if self._color.lower() == 'm' or self._color.lower() == 'magenta':
-            color = '\x1b[%s;40m' % (self.MAGENTA)
-        if self._color.lower() == 'c' or self._color.lower() == 'cyan':
-            color = '\x1b[%s;40m' % (self.CYAN)
-        if self._color.lower() == 'w' or self._color.lower() == 'white':
-            color = '\x1b[%s;40m' % (self.WHITE)
+        if self._color.lower().startswith('r'): color = '\x1b[%s;40m' % (RED)
+        if self._color.lower().startswith('g'): color = '\x1b[%s;40m' % (GREEN)
+        if self._color.lower().startswith('y'): color = '\x1b[%s;40m' % (YELLOW)
+        if self._color.lower().startswith('b'): color = '\x1b[%s;40m' % (BLUE)
+        if self._color.lower().startswith('m'): color = '\x1b[%s;40m' % (MAGENTA)
+        if self._color.lower().startswith('c'): color = '\x1b[%s;40m' % (CYAN)
+        if self._color.lower().startswith('w'): color = '\x1b[%s;40m' % (WHITE)
 
         return color
