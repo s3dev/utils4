824bee75ecd756b9097581e5cf5929b389a74240
j_berendt <development@s3dev.uk>
2021-12-14 12:48:54 +0000

    0.15.1 - Progress bar bug fix.
    progressbar.py:
    - BUG:
      - When run on Windows, the progress bar initialisation method
        re-initialised colorama for each instance of the progress bar.
      - This caused the progress bar to slow down substantially after
        about 200 iterations, and in some cases caused programs crashes
        due to a stack overflow.
    - FIX:
      - Moved the colorama initialisation into the __init__ module of
        the utils3 package, and only to initialise if on Windows.
    
    Released: 2021-12-14

--- build/lib/utils3/__init__.py
+++ build/lib/utils3/__init__.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import sys
 from utils3._version import __version__
 
+# Initialise colorama for Windows only.
+if sys.platform == 'win32':
+    import colorama
+    colorama.init()
--- utils3/progressbar.py
+++ utils3/progressbar.py
@@ -3,19 +3,15 @@
 :Purpose:   This is a small class module which provides access to a
             simple console progress bar.
 
-:Platform:  Linux/Windows | Python 3.5
+:Platform:  Linux/Windows | Python 3.5+
 :Developer: J Berendt
-:Email:     support@73rdstreetdevelopment.co.uk
+:Email:     support@s3dev.uk
 
 :Comments:  n/a
 
 """
-# pylint:disable=too-few-public-methods
-
-# TODO: Update to offer background colour.
 
 import sys
-import colorama
 
 
 class ProgressBar():
@@ -42,34 +38,30 @@ class ProgressBar():
     :Example:
         You might implement the progress bar in a loop like this::
 
-            import time
-            from utils3.progressbar import ProgressBar
+            >>> import time
+            >>> from utils3.progressbar import ProgressBar
 
-            pb = ProgressBar(total_values=25, bar_len=25, symbol='#',
-                             color='red')
+            >>> pb = ProgressBar(total_values=25, bar_len=25, symbol='#',
+                                 color='red')
 
-            # SOME PROCESS
-            for i range(26):
-                # UPDATE PROGRESS
-                pb.update_progress(current=i)
-                # OPTIONAL PAUSE TO SEE UPDATES
-                time.sleep(.1)
+            # Some process.
+            >>> for i range(26):
+            >>>    # < some processing >
+            >>>    # Update progress bar.
+            >>>    pb.update_progress(current=i)
+            >>>    # Optional pause to see updates.
+            >>>    time.sleep(.1)
 
     """
 
     def __init__(self, total_values=25, bar_len=25, symbol='.', color='w'):
-        """Class initialiser."""
-        # MAKE COLORS WORK WITH WINDOWS
-        colorama.init()
-        # SET CLASS VARIABLES
+        """Progress bar class initialiser."""
         self._total   = total_values
         self._bar_len = bar_len
         self._symbol  = symbol
         self._color   = color
         self._reset   = '\x1b[0m'
-        # GET USER'S COLOUR
         self._clr = self._getcolor()
-
 
     def update_progress(self, current):
         """Incrementally update the progress bar.
@@ -85,34 +77,31 @@ class ProgressBar():
             docstring.
 
         """
-        # CALCULATE PERCENT COMPLETE
+        # Calculate percent complete.
         percent = float(current) / self._total
-        # DETERMINE NUMBER OF 0 PLACEHOLDERS
+        # Determine number of 0 placeholders.
         vals = len(str(self._total))
-        # NUMBER OF TICKS
+        # Number of ticks.
         ticks = self._symbol * int(round(percent * self._bar_len))
-        # NUMBER OF SPACE PLACEHOLDERS
+        # Number of space placeholders.
         spaces = ' ' * (self._bar_len - len(ticks))
-
-        # PRINT OUTPUT
+        # Write output.
         sys.stdout.write(self._clr + '\rProcessing %s of %s [ %s ] %.0f%% Complete' % \
                          (str(current).zfill(vals),
                           self._total,
                           ticks + spaces,
                           percent*100) + self._reset)
-
-        # FLUSH BUFFER
+        # Flush buffer.
         sys.stdout.flush()
-
 
     def _getcolor(self) -> str:
         """Create ANSI colour escape sequence to user's colour.
 
         Returns:
-            ANSI escape sequence string for the user's colour.
+            str: ANSI escape sequence string for the user's colour.
 
         """
-        # ANSI COLOUR CONSTANTS
+        # ANSI colour constants.
         red     = '31'
         green   = '32'
         yellow  = '33'
@@ -120,9 +109,7 @@ class ProgressBar():
         magenta = '35'
         cyan    = '36'
         white   = '37'
-        # INITIALISE
         color = ''
-        # RETURN USER COLOR AN ANSI STRING
         if self._color.lower().startswith('r'): color = '\x1b[%s;40m' % (red)
         if self._color.lower().startswith('g'): color = '\x1b[%s;40m' % (green)
         if self._color.lower().startswith('y'): color = '\x1b[%s;40m' % (yellow)
